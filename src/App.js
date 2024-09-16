504fd4f611b4a8f6076510ed710b13b156951174
久光悠士 <hisamitsu@MacBook-Air.local>
2024-09-16 14:24:56 +0900

    message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello React app with AWS</h2>
+        <h3>welcome to yushi-app</h3>
       </header>
     </div>
   );
